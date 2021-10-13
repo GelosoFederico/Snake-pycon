98667783d7e81016d17c548fbf1268d601fb82c0
GelosoFederico <gelosofederico@gmail.com>
2021-10-13 00:20:32 -0300

    With tests

--- tests.py
+++ tests.py
@@ -12,8 +12,10 @@ in the folder where this file exists:
 """
 import unittest
 
-from server_logic import avoid_my_neck
+from server_logic import avoid_my_neck, create_empty_board
 
+def get_full_test_json():
+    return {  "game": {    "id": "game-00fe20da-94ad-11ea-bb37",    "ruleset": {      "name": "standard",      "version": "v.1.2.3"    },    "timeout": 500  },  "turn": 14,  "board": {    "height": 11,    "width": 11,    "food": [      {"x": 5, "y": 5},       {"x": 9, "y": 0},       {"x": 2, "y": 6}    ],    "hazards": [      {"x": 3, "y": 2}    ],    "snakes": [      {        "id": "snake-508e96ac-94ad-11ea-bb37",        "name": "My Snake",        "health": 54,        "body": [          {"x": 0, "y": 0},           {"x": 1, "y": 0},           {"x": 2, "y": 0}        ],        "latency": "111",        "head": {"x": 0, "y": 0},        "length": 3,        "shout": "why are we shouting??",        "squad": ""      },       {        "id": "snake-b67f4906-94ae-11ea-bb37",        "name": "Another Snake",        "health": 16,        "body": [          {"x": 5, "y": 4},           {"x": 5, "y": 3},           {"x": 6, "y": 3},          {"x": 6, "y": 2}        ],        "latency": "222",        "head": {"x": 5, "y": 4},        "length": 4,        "shout": "I'm not really sure...",        "squad": ""      }    ]  },  "you": {    "id": "snake-508e96ac-94ad-11ea-bb37",    "name": "My Snake",    "health": 54,    "body": [      {"x": 0, "y": 0},       {"x": 1, "y": 0},       {"x": 2, "y": 0}    ],    "latency": "111",    "head": {"x": 0, "y": 0},    "length": 3,    "shout": "why are we shouting??",    "squad": ""  }}
 
 class AvoidNeckTest(unittest.TestCase):
     def test_avoid_neck_all(self):
@@ -90,6 +92,13 @@ class AvoidNeckTest(unittest.TestCase):
         # Assert
         self.assertEqual(len(result_moves), 3)
         self.assertEqual(expected, result_moves)
+
+class MoveTest(unittest.TestCase):
+    def test_create_board(self):
+        board = create_empty_board(get_full_test_json()['board'])
+        assert len(board) == 11
+        for column in board:
+            assert len(column) == 11
 
 
 if __name__ == "__main__":
