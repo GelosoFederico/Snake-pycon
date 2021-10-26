db1f76bbfcc78f0b95fef4f85e69d7a51bb479a5
GelosoFederico <gelosofederico@gmail.com>
2021-10-26 00:59:19 -0300

    Select turns from latency

--- server_logic.py
+++ server_logic.py
@@ -207,7 +207,13 @@ def choose_move(data: dict) -> str:
     my_head = data["you"]["head"]  # A dictionary of x/y coordinates like {"x": 0, "y": 0}
 
     possible_moves = ["up", "down", "left", "right"]
-    possible_moves_next = remove_next_hazards(my_head, board, data, possible_moves, 3)
+    if data["you"]['latency'] > 450:
+        turns_ahead = 2
+    elif data["you"]['latency'] > 200:
+        turns_ahead = 3
+    else:
+        turns_ahead = 4
+    possible_moves_next = remove_next_hazards(my_head, board, data, possible_moves, turns_ahead)
     if len(possible_moves_next) == 0:
         possible_moves_next = possible_moves
 
